1f756fd8c6c03238ce157a4863d320d0378c3c46
michaelbarney <michaelbarneyjunior@gmail.com>
2022-09-05 21:04:27 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const axios = require("axios").default;
+
+const whatsAppToken = "EAAH3ozqtzPQBAMNaQ5igFoOBZBXVV5hO1GYJgqCSh1ZCa27ZCXeJwK9ZCkbZAFO0RphDAEF1aCv47RQEvItH4exSEN4cdKN6TOYNBLyYY4XXyC0FJLNtJDdYJLpaOtX95ZCcnRRXZATkWFvZAOtvGZB84XPVvT1YQCXPPAHbsFXSsySfiRkckCmYsEV22nkYEys8jjh7zN5eEkxSxGhbM6haC";
+
+exports.cloudAPI = (req, res) => {
+  const verify_token = "ABC";
+
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === verify_token) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+  // Check the Incoming webhook message
+  console.log(JSON.stringify(req.body, null, 2));
+
+  // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+  if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      axios({
+        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          whatsAppToken,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "Ack: " + msg_body },
+        },
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    res.sendStatus(200);
+  } else {
+    // Return a '404 Not Found' if event is not from a WhatsApp API
+    res.sendStatus(404);
+  }
+};
